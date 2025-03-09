5a92d8335de5ebcfc35e780cdf554aca388e2ada
Hakim.E <etahraouane.h@gmail.com>
2025-03-09 18:58:14 +0100

    Modifications avant application du stash

--- Main/obj/Debug/net9.0/Main.AssemblyInfo.cs
+++ Main/obj/Debug/net9.0/Main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+867e77f889be607eddda6d4633f9c086ece4693e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
