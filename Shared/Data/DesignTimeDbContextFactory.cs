b6fc34351a44a2bd0a9a8619d422085a3e25ef12
Hakim.E <etahraouane.h@gmail.com>
2025-04-03 00:20:56 +0200

    API_ORM_valider

--- Shared/Data/DesignTimeDbContextFactory.cs
+++ Shared/Data/DesignTimeDbContextFactory.cs
@@ -9,7 +9,7 @@ namespace Shared.Data
         {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
             // Mise à jour de la chaîne de connexion pour Docker Compose : Username=user
-            optionsBuilder.UseNpgsql("Host=127.0.0.1;Port=10000;Username=user;Password=123;Database=chess");
+            optionsBuilder.UseNpgsql( "Host=127.0.0.1;Port=10000;Username=ezchess;Password=123;Database=chess;");
             return new ApplicationDbContext(optionsBuilder.Options);
         }
     }
---
+++ Shared/Migrations/20250402135320_InitialCreate.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Formes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    position = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Formes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carres",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Cote = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carres", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Carres_Formes_Id",
+                        column: x => x.Id,
+                        principalTable: "Formes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cercles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Rayon = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cercles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cercles_Formes_Id",
+                        column: x => x.Id,
+                        principalTable: "Formes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rectangles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Longueur = table.Column<double>(type: "double precision", nullable: false),
+                    Largeur = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rectangles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rectangles_Formes_Id",
+                        column: x => x.Id,
+                        principalTable: "Formes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Triangles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Cote = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Triangles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Triangles_Formes_Id",
+                        column: x => x.Id,
+                        principalTable: "Formes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Carres");
+
+            migrationBuilder.DropTable(
+                name: "Cercles");
+
+            migrationBuilder.DropTable(
+                name: "Rectangles");
+
+            migrationBuilder.DropTable(
+                name: "Triangles");
+
+            migrationBuilder.DropTable(
+                name: "Formes");
+        }
+    }
+}
